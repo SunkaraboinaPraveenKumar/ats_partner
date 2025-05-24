85bdb9a9f7441efbd60988363ab9b9630bc7570b
SunkaraboinaPraveenKumar <sunkaraboinap@gmail.com>
2025-05-24 12:03:00 +0530

    ignore ts errors during build

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  typescript: {
+    // WARNING: this will let **any** TS error through in production builds!
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
