545d13561f102a2c706130a0300e81b9c7fb1d79
SunkaraboinaPraveenKumar <sunkaraboinap@gmail.com>
2025-06-08 21:55:29 +0530

    changes

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -28,7 +28,7 @@ const Header = () => {
   console.log(user);
   
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 p-3">
+    <header className="sticky top-0 z-50 w-full border-b bg-background p-3">
       <div className=" flex h-16 items-center justify-between">
         <Link href="/" className="flex items-center gap-2">
           <span className="font-bold text-xl">
